53acde13ea01a9bc7fbbf3a1792517cb82d5a172
vladaafanasjeva <73819965+vladaafanasjeva@users.noreply.github.com>
2022-05-02 10:04:02 +0200

    pls pls perfect perfect

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,12 +1,48 @@
-export default class navigationMenu {
-  constructor() {
-    this.template();
-  }
+export default class Dashboard{
+    constructor() {
+        this.template();
+        // this.dateDisplay();
+    }
 
-  template() {
-    document.querySelector("#home").innerHTML += /*html*/ `
+//     dateDisplay() {
+//     var objToday = new Date(),
+// 	weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+// 	dayOfWeek = weekday[objToday.getDay()],
+// 	domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "th"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" }(),
+// 	dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
+// 	months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+// 	curMonth = months[objToday.getMonth()],
+// 	curYear = objToday.getFullYear(),
+// 	curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
+// 	curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
+// 	curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(),
+// 	curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
+// var today = curHour + ":" + curMinute + "." + curSeconds + curMeridiem + " " + dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " + curYear;
+
+// document.getElementsByTagName('h1')[0].textContent = today;
+//     }
+
+    template() {
+      // program to display the date
+      // get local machine date time
+      const date = new Date();
+
+      // get the date as a string
+      const n = date.toDateString();
+
+      const today = `Today, ` + n
+
+      // const a = "day"
+
+      // const day = n[0].concat(a)
+
+      // display date
+      console.log(today);
+
+        document.querySelector("#home").innerHTML += /*html*/ `
      <h1 class="heading">DASHBOARD</h2>
 
+     <div class="layout">
       <div class="functions">
         <div class="function-box">
           <div class="function-content">
